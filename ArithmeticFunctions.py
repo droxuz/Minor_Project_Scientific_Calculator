ec99bf66af31aef27218d5a3a309cb39cc7b7c45
Andrew <andrewnong54@gmail.com>
2024-06-14 12:37:14 -0400

    Basic Arithmetic
    Added a class with the functionality to add, subtract, multiply, divide.

---
+++ ArithmeticFunctions.py
@@ -0,0 +1,20 @@
+class BasicArithmetic:
+
+    def addition(self, num1, num2):
+        sum= num1 + num2
+        return sum
+    
+    def subtraction(self, num1, num2):
+        difference = num1 - num2
+        return difference
+    
+    def multiplication(self, num1, num2):
+        product = num1 * num2
+        return product
+    
+    def division(self, num1, num2):
+        if num2 == 0:
+            raise ValueError("Cannot Divide by Zero. ")
+        quotient = num1 / num2
+        return quotient
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+from ArithmeticFunctions import BasicArithmetic
+basicMath = BasicArithmetic()
+result = basicMath.addition(4, 3)
+print(result)
+
+
