d1a9830fd3c14859c23505dcfcb6ae582ad36909
droxuz <156976891+droxuz@users.noreply.github.com>
2024-07-09 13:29:09 -0400

    Created Buttons for Arithmetic, and Added functions.

--- Interface.py
+++ Interface.py
@@ -13,12 +13,45 @@ frame.pack(pady=10, padx=10, fill="both", expand=True)
 root.resizable(False,False)
 
 #Creates the text box to input values
-CalculationFieldTextBox = customtkinter.CTkTextbox(frame, height=50, width=460, font=("Roboto",26))
-CalculationFieldTextBox.pack(fill="both", expand=True)
-CalculationFieldTextBox.place(x=10, y=10)
-CalculationFieldTextBox.insert('0.0', 'new text to insert')  # insert at line 0 character 0
-text = CalculationFieldTextBox.get('0.0', 'end')  # get text from line 0 character 0 till the end
-CalculationFieldTextBox.delete('0.0', 'end')  # delete all text
-CalculationFieldTextBox.configure(state='normal')  # configure textbox to be read-only
+entry = customtkinter.CTkEntry(frame, placeholder_text='', width=460, height=50, font=("Roboto",26))
+entry.place(x=10, y=10)
+
+#Creates the function for buttons
+def division_func():
+        box_content = entry.get()
+        if box_content and box_content[-1].isdigit():
+            entry.insert(customtkinter.END,chr(247))
+        #entry.insert(root.END," "+chr(247)+" ")
+
+def multiplication_button():
+    box_content = entry.get()
+    for index, char in enumerate(box_content):
+        if char.isdigit():
+            entry.insert(index+1," "+"×"+" ")
+            break
+    else:
+        entry.insert(1," "+"×"+" ")
+
+def subtraction_button():
+    entry.insert(-1," "+"-"+" ")
+
+def addition_button():
+    entry.insert(-1," "+"+"+" ")
+
+#Creates Division Button
+Division_Button = customtkinter.CTkButton(frame, command=division_func,text="÷", width=140, height=50, font=("Roboto", 26))
+Division_Button.place(x=330, y=60)
+
+#Creates Multiplication Button
+Multiplication_Button = customtkinter.CTkButton(frame, command=multiplication_button, text="×", width=140, height=50, font=("Roboto", 26))
+Multiplication_Button.place(x=330, y=110)
+
+#Creates Subtraction Button
+Subtraction_Button = customtkinter.CTkButton(frame, command=subtraction_button, text='-', width=140, height=50, font=("Roboto", 26))
+Subtraction_Button.place(x=330, y=160)
+
+#Creates Addition Button
+Addition_Button = customtkinter.CTkButton(frame, command=addition_button, text='+', width=140, height=50, font=("Roboto", 26))
+Addition_Button.place(x=330, y=210)
 
 root.mainloop()
